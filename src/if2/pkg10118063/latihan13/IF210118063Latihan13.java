30f4a91980fd19c84bbf0b6e6f46affe6908b2bf
NaufalAshaW <55681676+NaufalAshaW@users.noreply.github.com>
2019-09-23 19:59:58 +0700

    Add files via upload

---
+++ src/if2/pkg10118063/latihan13/IF210118063Latihan13.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if2.pkg10118063.latihan13;
+
+/**
+ *
+ * @author 
+ * NAMA   : Naufal Asha
+ * KELAS  : IF-2
+ * NIM    : 10118063
+ */
+public class IF210118063Latihan13 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int bil1 = 10;
+        int bil2 = 20;
+        System.out.println("a == b = " + (bil1 == bil2));
+        System.out.println("a != b = " + (bil1 != bil2));
+        System.out.println("a > b = " + (bil1 > bil2));
+        System.out.println("a < b = " + (bil1 < bil2));
+        System.out.println("b >= a = " +(bil2 >= bil2));
+        System.out.println("b <= a = " + (bil2 <= bil1));
+    }
+    
+}
